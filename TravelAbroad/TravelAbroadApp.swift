9bff7cceefbc8f44da676f8dbdf997c44a4fcf52
TannerM03 <tanner.macpherson3@gmail.com>
2025-05-19 17:10:40 -0400

    Initial Commit

---
+++ TravelAbroad/TravelAbroadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAbroadApp.swift
+//  TravelAbroad
+//
+//  Created by Tanner Macpherson on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelAbroadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
