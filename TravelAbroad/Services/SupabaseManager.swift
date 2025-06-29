e2553f07e176d9ea530224ce51fb20580f5c1103
TannerM03 <tanner.macpherson3@gmail.com>
2025-06-29 00:07:58 -0700

    create stage

--- TravelAbroad/Services/SupabaseManager.swift
+++ TravelAbroad/Services/SupabaseManager.swift
@@ -16,6 +16,7 @@ class SupabaseManager {
         supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dHRnenJxbnR5emVoZnVmZXF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MjIzNDEsImV4cCI6MjA2Mjk5ODM0MX0.B_kWPnoSjiENKIfggFqMWzdu_vdnKML1gJmzVy4NCcs"
     )
     
+    
     func fetchCities() async throws -> [City] {
         print("service")
         let cities: [City] = try await supabase.from("cities")
