4a6e1f42cbf2d7ebff3fa97dd70e3572208e872e
TannerM03 <tanner.macpherson3@gmail.com>
2025-07-09 21:46:54 -0700

    move api keys

--- TravelAbroad/Services/SupabaseManager.swift
+++ TravelAbroad/Services/SupabaseManager.swift
@@ -13,8 +13,8 @@ class SupabaseManager {
     static let shared = SupabaseManager()
 
     let supabase = SupabaseClient(
-        supabaseURL: URL(string: "https://tyttgzrqntyzehfufeqx.supabase.co")!,
-        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5dHRnenJxbnR5emVoZnVmZXF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MjIzNDEsImV4cCI6MjA2Mjk5ODM0MX0.B_kWPnoSjiENKIfggFqMWzdu_vdnKML1gJmzVy4NCcs"
+        supabaseURL: URL(string: ConfigManager.shared.supabaseURL)!,
+        supabaseKey: ConfigManager.shared.supabaseKey
     )
 
     // fetches the cities to be displayed as travel options,
